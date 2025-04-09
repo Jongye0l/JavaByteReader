aa8bd0acff0d3a683ce6b6c1d8539ade4c47edb8
Jongyeol <bcpjy1233@gmail.com>
2025-04-09 10:33:51 +0900

    refactor: JavaCoe Sort Number

--- JavaByteReader/Emit/JavaOpCode.cs
+++ JavaByteReader/Emit/JavaOpCode.cs
@@ -1,12 +1,21 @@
 ﻿namespace JavaByteReader.Emit;
 
 public sealed class JavaOpCode {
-    public readonly string Name;
-    public readonly JavaCode Code;
+    public readonly string name;
+    public readonly JavaCode code;
+    public readonly byte instructionSize;
+    public readonly byte getStackSize;
+    public readonly byte setStackSize;
 
     public JavaOpCode(string name, JavaCode code) {
-        Name = name;
-        Code = code;
+        this.name = name;
+        this.code = code;
         JavaOpCodes.OpCodes[(int)code] = this;
+    }
+
+    public JavaOpCode(string name, JavaCode code, byte instructionSize, byte getStackSize, byte setStackSize) : this(name, code) {
+        this.instructionSize = instructionSize;
+        this.getStackSize = getStackSize;
+        this.setStackSize = setStackSize;
     }
 }
